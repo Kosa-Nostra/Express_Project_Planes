a069d32a837db66bd7ed8887af7cea3af9c53ecc
Ayder <muzdabaev252@gmail.com>
2024-10-30 10:42:12 +0300

    plane_4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* Страница A320*/
+router.get('/a320', function(req, res, next) {
+  res.send("<h1>Airbus A320</h1>")
+  });
+  /* Страница C5*/
+router.get('/c5', function(req, res, next) {
+  res.send("<h1>C5 Galaxy</h1>")
+  });
+  /* Страница boeing 747*/
+router.get('/boeing747', function(req, res, next) {
+  res.send("<h1>Boeing 747</h1>")
+  });
 
 module.exports = router;
