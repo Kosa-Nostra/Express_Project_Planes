1d175029f22a3858a88f0eb6f19685f127ce9192
Ayder <muzdabaev252@gmail.com>
2024-10-31 12:18:48 +0300

    Bootstrap fix

--- createDB.js
+++ createDB.js
@@ -5,13 +5,13 @@ const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 // Database Name
-const dbName = 'test2024';
+const dbName = 'Plane';
 async function main() {
 // Use connect method to connect to the server
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
-    const collection = db.collection('documents');
+    const collection = db.collection('Planes');
     // the following code examples can be pasted here...
     const insertResult = await collection.insertMany([{ a: 1 }, { a:2 }, { a: 3 }]);
     console.log('Inserted documents =>', insertResult);
